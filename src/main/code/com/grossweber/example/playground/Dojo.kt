6def2ec6ab9474fe74abe4e9b3cf691b5a4c9528
Felix Bytow <felix@staffbase.com>
2018-07-11 10:33:34 +0200

    implemented parts of dojo task

--- src/main/code/com/grossweber/example/playground/Dojo.kt
+++ src/main/code/com/grossweber/example/playground/Dojo.kt
@@ -1,9 +1,45 @@
 package com.grossweber.example.playground
 
-class Dojo {
+class Dojo(val memory: IntArray = IntArray(MEMORY_SIZE),
+  var currentField: Int = 0,
+  var output: String = "") {
+
+
   companion object {
+    const val MEMORY_SIZE = 65536
+
     fun interpret(sourceCode: String): String {
       TODO()
     }
   }
+
+  fun incrementCurrentField() {
+    ++memory[currentField]
+  }
+
+  fun decrementCurrentField() {
+    --memory[currentField]
+    if(currentField < 0) {
+      currentField = MEMORY_SIZE-1
+    }
+  }
+
+  fun incrementCurrentPosition() {
+    ++currentField
+    if (currentField == MEMORY_SIZE){
+      currentField = 0
+    }
+  }
+
+  fun decrementCurrentPosition() {
+    --currentField
+    if (currentField == -1){
+      currentField = MEMORY_SIZE-1
+    }
+  }
+
+  fun appendToOutut() {
+    output = output + memory[currentField].toChar()
+  }
+
 }
